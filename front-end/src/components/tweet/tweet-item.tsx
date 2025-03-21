6b8f7d096b5accf01edc8595b0db36e3ea07a951
warley farias <157419864+warrley@users.noreply.github.com>
2025-03-21 16:10:32 -0300

    hideComent op

--- front-end/src/components/tweet/tweet-item.tsx
+++ front-end/src/components/tweet/tweet-item.tsx
@@ -11,7 +11,7 @@ import { useState } from "react"
 
 type Props = {
     tweet: Tweet
-    hideComments: boolean;
+    hideComments?: boolean;
 }
 
 export const TweetItem = ({ tweet, hideComments }: Props) => {
