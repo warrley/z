d06d3baa1b3a433ff31c372f3cd50acb76611159
warley farias <157419864+warrley@users.noreply.github.com>
2025-03-21 16:35:05 -0300

    finish

--- front-end/src/app/(ui)/layout.tsx
+++ front-end/src/app/(ui)/layout.tsx
@@ -10,7 +10,7 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: {children: ReactNode}) {
     return (
-        <main className="min-h-screen flex justify-center mx-auto max-w-7xl">
+        <main className="min-h-screen flex justify-between mx-auto ">
             <section className="hidden lg:flex flex-col sticky top-0 left-0 h-screen w-72 px-3 border-r-2 border-gray-900">
                 <div className="flex-1 mt-6">
                     <Logo size={24} />
@@ -33,7 +33,7 @@ export default function Layout({ children }: {children: ReactNode}) {
                     <NavMyProfile/>
                 </div>
             </section>
-            <section className="flex-1 max-w-lg">
+            <section className="flex-1 max-w-4xl">
                 {children}
             </section>
             <aside className="hidden lg:flex flex-col gap-6 sticky top-0 left-0 h-fit w-96 px-8 py-6 border-l-2 border-gray-900">
