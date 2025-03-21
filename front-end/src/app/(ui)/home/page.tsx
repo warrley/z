784e2bb2f8fbc96b55295b385eb50128aa039f8b
warley farias <157419864+warrley@users.noreply.github.com>
2025-03-21 12:52:23 -0300

    recommendation | tweet

--- front-end/src/app/(ui)/home/page.tsx
+++ front-end/src/app/(ui)/home/page.tsx
@@ -1,10 +1,10 @@
 import { HomeHeader } from "@/components/home/home-header";
-import { TweetPost } from "@/components/tweet/twee-post";
+import { TweetPost } from "@/components/tweet/tweet-post";
 
 export default function Page() {
     return (
         <div>
-            <HomeHeader />
+            <HomeHeader/>
             <TweetPost/>
         </div>
     )
