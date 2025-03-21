81b58bb7c1261232499f027ad6c40193e7323dba
warley farias <157419864+warrley@users.noreply.github.com>
2025-03-21 15:24:14 -0300

    profile info

--- front-end/src/app/(ui)/[slug]/page.tsx
+++ front-end/src/app/(ui)/[slug]/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
 import { GeneralHeader } from "@/components/ui/general-header";
 import { user } from "@/data/user";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 export default function Page() {
+    const isMe = true;
+
     return (
         <div>
             <GeneralHeader backHref="/">
@@ -10,6 +16,43 @@ export default function Page() {
             </GeneralHeader>
             <section className="border-b-2 border-gray-900">
                 <div className="bg-gray-500 h-28 bg-no-repeat bg-cover bg-center" style={{ backgroundImage: 'url(' + user.cover + ')' }}></div>
+                <div className="-mt-12 flex justify-between items-end px-6">
+                    <img
+                        src={user.avatar}
+                        alt={user.name}
+                        className="size-24 rounded-full"
+                    />
+                    <div className="w-32">
+                        {isMe ?
+                            <Link href={`/${user.slug}/edit`}>
+                                <Button
+                                    label="Edit Profile"
+                                    size={2}
+                                />
+                            </Link>
+                            :
+                            <Button
+                                label="Follow"
+                                size={2}
+                            />
+                        }
+                    </div>
+                </div>
+                <div className="px-6 mt-4">
+                    <div className="text-xl font-bold">{user.name}</div>
+                    <div className="text-gray-500">@{user.slug}</div>
+                    <div className="py-5 text-lg text-gray-500">{user.bio}</div>
+                    {user.link && 
+                        <div className="flex gap-2 items-center">
+                            <FontAwesomeIcon icon={faLink} className="text-lg"/>
+                            <Link className="text-blue-300" target="_blank" href={user.link}>{user.link}</Link>
+                        </div>
+                    } 
+                    <div className="my-5 flex gap-6">  
+                        <div className="text-xl text-gray-500"><span className="text-white">99</span> Following</div>
+                        <div className="text-xl text-gray-500"><span className="text-white">99</span> Folower</div>
+                    </div>
+                </div>
             </section>
         </div>
     )
